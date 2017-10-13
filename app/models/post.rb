4789e05ce439d967f6cab65babd8e50966cc1e64
Raja Sharma <rajasharma55@gmail.com>
2017-10-13 00:58:18 -0400

    Apply styling. Add instance methods to Post model.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
 
   has_many :votes
 
+  def points
+    votes.sum(:value)
+  end
+
+  def time_since_creation
+    ((Time.now - created_at) / 3600).round
+  end
+
 end
